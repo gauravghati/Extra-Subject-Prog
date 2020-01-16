58b176311f7ed59f1ae27dba5ce0469773c5f6e9
gauravghati <gauravghati225@gmail.com>
2020-01-16 22:26:50 +0530

    PAI And CG added

--- cc.cpp
+++ cc.cpp
@@ -1,23 +1,38 @@
 # include <bits/stdc++.h>
 typedef long long ll;
 # define pb push_back
-# define for(i, a, b) for(int i=a; i<b; i++)
+# define rep(i, a, b) for(int i=a; i<b; i++)
+# define repr(i, a, b) for(int i=a; i>=b; --i)
+# define  ff  first
+# define  ss  second
+# define fastt ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+# define testt int T; cin>>T; while(T--)
+# define all(v) v.begin(), v.end()
+# define allr(v) v.rbegin(), v.rend()
+# define vll vector<ll>
+# define vii vector<int>
+# define pll pair<ll, ll> 
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin >> t;
-    while(t--){
+    fastt;
+	//testt{
     	// write your code here.
     	
+    	int N;
+    	cin >> N;
     	
+    	int flag = 0;
+    	rep(i, 1, N){
+    		if((i%2==0) && ((N-i)%2==0)){
+    			flag = 1;
+    		}
+    	}
     	
-    }
+    	if(flag==0) cout << "NO";
+    	else cout << "YES";
+    	
+	//}		
     return 0;
-
 }
-
-
 
